88169e76cad085c90e6ed217ba79ed3c3bcffdbc
Sanika1612 <117501602+Sanika1612@users.noreply.github.com>
2024-03-08 10:42:02 +0530

    Commit before adding responsiveness

---
+++ main.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.querySelector(".navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+var typed=new Typed(".text", {
+    strings: ["Programmer", "Developer", "Learner"],
+    typeSpeed:100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop:true
+})
+// JavaScript function to toggle the dropdown menu
+
